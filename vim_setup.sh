ba93ef0d560548ef80dcace3ab461d08a0048c9c
Adrian <withoutpointk@gmail.com>
2013-05-27 20:40:11 -0700

    hmmmm

---
+++ vim_setup.sh
@@ -0,0 +1,18 @@
+# Clean
+rm -rf ~/.vim
+
+# Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+# NERDTree
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
+
+# CtrlP
+cd ~/.vim/bundle
+git clone https://github.com/kien/ctrlp.vim.git
+
+# Solarized
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
