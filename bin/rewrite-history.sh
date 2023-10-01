88d6dbc5d8b8bc44f6f79de041c22d75d7b37e9a
Raekye <Raekye@users.noreply.github.com>
2023-10-01 15:00:17 -0400

    Hmmm...
    - Change vim plugins to use 'fzf' instead of 'ctrlp' and 'ack/ag'.
    - Update git history rewriting script...
    - Update git config to only allow fast-forward pulls.

--- bin/rewrite-history.sh
+++ bin/rewrite-history.sh
@@ -4,6 +4,15 @@ set -e
 
 #git show --no-patch --format='%ad' HEAD
 
-git rebase --root --rebase-merges --exec "git commit --amend --no-edit --author '$(git config user.name) <$(git config user.email)>'"
+# https://git-scm.com/book/en/v2/Git-Internals-Environment-Variables
+if test -z "$GIT_AUTHOR_NAME"; then
+	GIT_AUTHOR_NAME="$(git config user.name)"
+fi
+if test -z "$GIT_AUTHOR_EMAIL"; then
+	GIT_AUTHOR_EMAIL="$(git config user.email)"
+fi
 
-git rebase --root --rebase-merges --committer-date-is-author-date
+# Use --rebase-merges to preserve merge commits; otherwise, by default, merge commits will be dropped.
+git rebase --root --exec "git commit --amend --no-edit"
+
+git rebase --root --committer-date-is-author-date
